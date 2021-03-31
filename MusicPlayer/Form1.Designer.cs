e14aa5456f036545b3a9098243bf4d346027420f
Ramon Frias <ramonfrias2451@gmail.com>
2021-03-31 20:52:49 -0300

    Musica or Video Player

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace MusicPlayer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listPlayList = new System.Windows.Forms.ListBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.ofOpen = new System.Windows.Forms.OpenFileDialog();
+            this.ofSave = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(12, 12);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(475, 274);
+            this.player.TabIndex = 0;
+            this.player.Enter += new System.EventHandler(this.player_Enter);
+            // 
+            // listPlayList
+            // 
+            this.listPlayList.FormattingEnabled = true;
+            this.listPlayList.Location = new System.Drawing.Point(12, 292);
+            this.listPlayList.Name = "listPlayList";
+            this.listPlayList.Size = new System.Drawing.Size(475, 147);
+            this.listPlayList.TabIndex = 1;
+            this.listPlayList.DoubleClick += new System.EventHandler(this.listPlayList_DoubleClick);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 445);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(105, 445);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(196, 445);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(287, 445);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 477);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.listPlayList);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.ListBox listPlayList;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog ofOpen;
+        private System.Windows.Forms.SaveFileDialog ofSave;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MusicPlayer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void player_Enter(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        //Variavel da playlist
+        WMPLib.IWMPPlaylist playList;
+
+        private void button1_Click(object sender, EventArgs e) {
+            //Titulo do botão
+            ofOpen.Title = "Abrir mídia";
+            //Filtro
+            ofOpen.Filter = "Arquivo mp4|*.mp4|Arquivo mp3|*.mp3";
+            //Caixa de dialogo para o usuario selecionar os arquivos de audio ou de video
+            //Se o usuario selecionou um arquivo
+            if (ofOpen.ShowDialog() == DialogResult.OK) {
+                //Após selecionar todo que ira ser colocado na playlist
+                playList = player.playlistCollection.newPlaylist("List");
+                //Para cada arquivo selecionado na playlist
+                foreach (var arquivo in ofOpen.FileNames) {
+                    //Add cada item para a playlist
+                    playList.appendItem(player.newMedia(arquivo));
+                    //Add na playlist
+                    listPlayList.Items.Add(arquivo);
+                    //Dando o play na execução
+                    player.currentPlaylist = playList;
+                    player.Ctlcontrols.play();
+                }
+            }
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e) {
+            //Se na playlist tiver arquivos
+            if (listPlayList.Items.Count > 0) {
+                //Caixa de dialogo salvar
+                ofSave.Title = "Salvar Playlist";
+                ofSave.Filter = "Arquivo texto|*.txt";
+                //Se tiver arquivos para salvar
+                if (ofSave.ShowDialog() == DialogResult.OK) {
+                    //Escrever um arquivo texto e salva, o false é para que se tiver um arquivo com o nome ele sobrescreve
+                    StreamWriter arquivo = new StreamWriter(ofSave.FileName, false);
+                    //Salvar a lista, varrendo o ListBox salvando no arquivo
+                    for (int i = 0; i < listPlayList.Items.Count; i++) {
+                        //Adiconando dentro do arquivo
+                        arquivo.WriteLine(listPlayList.Items[i].ToString());
+                    }
+                    //Sempre fechar o arquivo depois de sair da opção
+                    arquivo.Close();
+                }
+
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e) {
+            //Titulo do botão
+            ofOpen.Title = "Abrir Playlist";
+            //Filtro
+            ofOpen.Filter = "Arquivo texto|*.txt";
+            //
+            ofOpen.Multiselect = false;
+            //Se abrir o  arquivo
+            if (ofOpen.ShowDialog() == DialogResult.OK) {
+                //Le o arquivo
+                StreamReader arquivo = new StreamReader(ofOpen.FileName);
+                //Coloca o arquivo na ListBox
+                while (arquivo.Peek() != -1) {
+                    //Adiconando na playlist
+                    listPlayList.Items.Add(arquivo.ReadLine());
+                }
+                //Sempre fechar o arquivo depois de sair da opção
+                arquivo.Close();
+            }
+        }
+
+        private void listPlayList_DoubleClick(object sender, EventArgs e) {
+            //Se tem item na lista
+            if (listPlayList.Items.Count > 0) {
+                //Executando o arquivo
+                player.URL = listPlayList.SelectedItem.ToString();
+                player.Ctlcontrols.play();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) {
+            //Se tiver executando algo ja da um stop
+            player.Ctlcontrols.stop();
+            listPlayList.Items.Clear();
+        }
+    }
+}
